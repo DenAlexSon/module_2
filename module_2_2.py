c880a14c3c5651e5e0838d2701e54029b714217b
DenAlexSon <den.xxx@list.ru>
2024-07-06 09:28:08 +0300

    Initial commit

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = int(input('введите первое целое число: '))
+second = int(input('введите второе целое число: '))
+third = int(input('введите третье целое число: '))
+if first==second==third:
+    print(3)
+elif first==second or second==third or first==third:
+    print(2)
+else:
+    print(0)
